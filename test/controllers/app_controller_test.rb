cbdd9c75780b1a2aecbdf1784da9319dda5bcd0a
Tom Kruijsen <tom@brightin.nl>
2017-12-19 18:07:19 +0100

    Move some things around

--- test/controllers/app_controller_test.rb
+++ test/controllers/app_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class AppControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get app_index_url
+    get root_url
     assert_response :success
   end
 
